7e8660a478caeee0aa54a33921a3f4eb77add351
mingming0865 <mingming0865@gmail.com>
2025-01-17 02:05:20 +0700

    Register complete

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,9 +1,8 @@
 import './Register.scss';
 import { useHistory } from "react-router-dom";
-import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
-
+import { registerNewUser } from '../../services/userService';
 const Register = (prop) => {
     const [email, setEmail] = useState("");
     const [phone, setPhone] = useState("");
@@ -71,16 +70,20 @@ const Register = (prop) => {
         return true;
     }
 
-    const handleRegister = () => {
+    const handleRegister = async () => {
 
         let check = isValidInputs();
 
         if (check === true) {
-            axios.post("http://localhost:8080/api/v1/register", {
-                email, phone, username, password
-            })
+            let response = await registerNewUser(email, phone, username, password);
+            let serverData = response.data;
+            if (+serverData.EC === 0) {
+                toast.success(serverData.EM)
+                history.push("/login");
+            } else {
+                toast.error(serverData.EM)
+            }
         }
-
     }
 
     return (
