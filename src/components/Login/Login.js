1ca547b36074495ca6331a73071c74cc73fb2e37
mingming0865 <mingming0865@gmail.com>
2025-01-16 15:36:07 +0700

    form login responsive

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,10 +2,10 @@ import './Login.scss';
 
 const Login = (prop) => {
     return (
-        <div className="login-container mt-3">
+        <div className="login-container">
             <div className="container">
-                <div className="row">
-                    <div className="content-left col-7">
+                <div className="row px-3 px-sm-0">
+                    <div className="content-left col-12 d-none col-sm-7 d-sm-block">
                         <div className="brand">
                             Ming
                         </div>
@@ -13,17 +13,21 @@ const Login = (prop) => {
                             Ming helps you connect and share with the people in your life.
                         </div>
                     </div>
-                    <div className="content-right col-5 green d-flex flex-column gap-3 py-3">
+                    <div className="content-right col-12 col-sm-5 d-flex flex-column gap-3 py-3 ">
+                        <div className="brand d-sm-none">
+                            Ming
+                        </div>
                         <input type="text" className='form-control' placeholder='Email address or phone number' />
                         <input type="password" className='form-control' placeholder='Password' />
                         <button className='btn btn-primary'>Login</button>
-                        <span className='text-center'>Forgot your password?</span>
+                        <span className='text-center'>
+                            <a className='forgot-password' href='#'>Forgot your password?</a>
+                        </span>
                         <hr />
                         <div className='text-center'>
                             <button className='btn btn-success'>Create new account</button>
                         </div>
                     </div>
-
                 </div>
             </div>
         </div>
