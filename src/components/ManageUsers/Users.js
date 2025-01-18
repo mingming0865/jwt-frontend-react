49ef39fb185b743c206b8ba312b2c9f7fc4de95a
mingming0865 <mingming0865@gmail.com>
2025-01-18 23:19:08 +0700

    display list users

--- src/components/ManageUsers/Users.js
+++ src/components/ManageUsers/Users.js
@@ -1,13 +1,84 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
+
 import './Users.scss';
+import { fetchAllUser } from "../../services/userService";
 
 const Users = (props) => {
+    const [listUsers, setListUsers] = useState([]);
+
+    useEffect(() => {
+        fetchUsers();
+    }, [])
+
+    const fetchUsers = async () => {
+        let responce = await fetchAllUser();
+        if (responce && responce.data && responce.data.EC === 0) {
+            setListUsers(responce.data.DT);
+            console.log(responce.data.DT)
+        }
+    }
 
     return (
-        <div>
-            users component
+        <div className="container">
+            <div className="manage-users-container">
+                <div className="user-header">
+                    <div className="title">
+                        <h3>Table Users</h3>
+                    </div>
+                    <div className="actions">
+                        <button className="btn btn-success">Refesh</button>
+                        <button className="btn btn-primary">Add new user</button>
+                    </div>
+                </div>
+                <div className="user-body">
+                    <table class="table table-bordered table-hover">
+                        <thead>
+                            <tr>
+                                <th scope="col">No</th>
+                                <th scope="col">Id</th>
+                                <th scope="col">Email</th>
+                                <th scope="col">Username</th>
+                                <th scope="col">Group</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {listUsers && listUsers.length > 0 ?
+                                <>
+                                    {listUsers.map((item, index) => {
+                                        return (
+                                            <tr key={`row-${index}`}>
+                                                <td>{index + 1}</td>
+                                                <td>{item.id}</td>
+                                                <td>{item.email}</td>
+                                                <td>{item.username}</td>
+                                                <td>{item.group ? item.Group.name : ''}</td>
+                                            </tr>
+                                        )
+                                    })}
+                                </>
+                                :
+                                <>
+                                    <span>Not found users</span>
+                                </>
+                            }
+                        </tbody>
+                    </table>
+                </div>
+                <div className="user-footer">
+                    <nav aria-label="Page navigation example">
+                        <ul class="pagination">
+                            <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+                            <li class="page-item"><a class="page-link" href="#">1</a></li>
+                            <li class="page-item"><a class="page-link" href="#">2</a></li>
+                            <li class="page-item"><a class="page-link" href="#">3</a></li>
+                            <li class="page-item"><a class="page-link" href="#">Next</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
         </div>
-    )
-}
+
+    );
+};
 
 export default Users;
