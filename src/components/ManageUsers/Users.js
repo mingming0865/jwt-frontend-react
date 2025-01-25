ad5c08aa0ded8c5cd71987c930325d1d8a83bae3
mingming0865 <mingming0865@gmail.com>
2025-01-25 16:07:19 +0700

    Modal Create User

--- src/components/ManageUsers/Users.js
+++ src/components/ManageUsers/Users.js
@@ -4,6 +4,7 @@ import { fetchAllUser, deleteUser } from "../../services/userService";
 import ReactPaginate from 'react-paginate';
 import { toast } from "react-toastify";
 import ModalDelete from "./ModalDelete";
+import ModalUser from "./ModalUser";
 
 const Users = (props) => {
     const [listUsers, setListUsers] = useState([]);
@@ -141,6 +142,11 @@ const Users = (props) => {
                 confirmDeleteUser={confirmDeleteUser}
                 dataModal={dataModal}
             />
+
+            <ModalUser
+                title={"Create new user"}
+            />
+
         </>
 
 
