693608869bf827c7b5f589d1d8001509407574e9
mingming0865 <mingming0865@gmail.com>
2025-01-29 22:13:53 +0700

    crud complete

--- src/components/ManageUsers/Users.js
+++ src/components/ManageUsers/Users.js
@@ -73,22 +73,33 @@ const Users = (props) => {
         setIsShowModalUser(true);
     }
 
+    const handleRefresh = async () => {
+        await fetchAllUser();
+    }
+
     return (
         <>
             <div className="container">
                 <div className="manage-users-container">
                     <div className="user-header">
-                        <div className="title">
-                            <h3>Table Users</h3>
+                        <div className="title mt-3">
+                            <h3>Manage Users</h3>
                         </div>
-                        <div className="actions">
-                            <button className="btn btn-success">Refesh</button>
+                        <div className="actions my-3">
+                            <button
+                                className="btn btn-success refresh"
+                                onClick={() => handleRefresh()}
+                            >
+                                <i className="fa fa-refresh" ></i>Refesh</button>
                             <button className="btn btn-primary"
                                 onClick={() => {
                                     setIsShowModalUser(true);
                                     setActionModalUser("CREATE");
                                 }}
-                            >Add new user</button>
+                            >
+                                <i className="fa fa-plus-circle" ></i>
+                                Add new user
+                            </button>
                         </div>
                     </div>
                     <div className="user-body">
@@ -114,12 +125,18 @@ const Users = (props) => {
                                                     <td>{item.username}</td>
                                                     <td>{item.Group ? item.Group.name : ''}</td>
                                                     <td>
-                                                        <button className="btn btn-warning mx-3"
+                                                        <span
+                                                            title="Edit"
+                                                            className="edit"
                                                             onClick={() => handleEditUser(item)}
-                                                        >Edit</button>
-                                                        <button className="btn btn-danger"
+                                                        >
+                                                            <i class="fa fa-pencil"></i>
+                                                        </span>
+                                                        <span
+                                                            title="Delete"
+                                                            className="delete"
                                                             onClick={() => handleDeleteUser(item)}
-                                                        >Delete</button>
+                                                        ><i class="fa fa-trash-o"></i></span>
                                                     </td>
                                                 </tr>
                                             )
